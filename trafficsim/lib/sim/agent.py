783bb7fef9280fc473ff8d9d50d8cc22f7d23141
unknown <maartenjeu@gmail.com>
2021-12-11 12:37:38 +0100

    Add logic between agent & model

--- trafficsim/lib/sim/agent.py
+++ trafficsim/lib/sim/agent.py
@@ -9,3 +9,15 @@ class Car(mesa.Agent):
     def __init__(self, m: model.World) -> None:
         super().__init__(m.next_id(), m)
         # Add speed and other attributes here.
+
+    def step(self) -> None:
+        raise NotImplementedError()
+
+    def advance(self) -> None:
+        raise NotImplementedError()
+
+    def perceive(self) -> None:
+        raise NotImplementedError()
+
+    def act(self) -> None:
+        raise NotImplementedError()
--- trafficsim/lib/sim/model.py
+++ trafficsim/lib/sim/model.py
@@ -1,6 +1,9 @@
 import mesa
 import mesa.time
 import mesa.space
+import numpy as np
+
+import agent
 
 
 class World(mesa.Model):
@@ -8,8 +11,24 @@ class World(mesa.Model):
 
     Whether this is a lane, or a road, is yet to be determined."""
 
-    def __init__(self, width: int, height: int) -> None:
+    def __init__(self, width: int,
+                 amount_of_agents: int) -> None:
         """Initialize class instance."""
 
+        # Initialize world parameters
         self.schedule = mesa.time.SimultaneousActivation(self)
-        self.grid = mesa.space.SingleGrid(width, height, torus=True)
+        self.grid = mesa.space.SingleGrid(width, 1, torus=True)
+
+        # Initialize agents
+        if amount_of_agents > width:
+            raise ValueError(f"Amount of agents {amount_of_agents} may not exceed world width {width}.")
+
+        agent_locations: np.ndarray = np.linspace(0, width, amount_of_agents, endpoint=False).astype(int)
+
+        for loc in agent_locations:
+            a = agent.Car(self)
+            self.grid.place_agent(a, (loc, 0))
+
+
+    def step(self) -> None:
+        raise NotImplementedError()
