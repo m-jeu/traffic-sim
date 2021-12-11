c961373854ab7b3206fdc2137669964fff0cf181
unknown <maartenjeu@gmail.com>
2021-12-11 12:12:09 +0100

    Add simple Car __init__

--- trafficsim/lib/sim/agent.py
+++ trafficsim/lib/sim/agent.py
@@ -1,1 +1,11 @@
 import mesa
+
+import model
+
+
+class Car(mesa.Agent):
+    """Vroom vroom."""
+
+    def __init__(self, m: model.World) -> None:
+        super().__init__(m.next_id(), m)
+        # Add speed and other attributes here.
