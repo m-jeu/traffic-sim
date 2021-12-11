7b9e9b9bfd77447abce37887f71344e4f0338cfa
unknown <maartenjeu@gmail.com>
2021-12-11 12:05:52 +0100

    Add simple World __init__

--- trafficsim/lib/sim/model.py
+++ trafficsim/lib/sim/model.py
@@ -1,1 +1,15 @@
 import mesa
+import mesa.time
+import mesa.space
+
+
+class World(mesa.Model):
+    """A world, in which vehicles move.
+
+    Whether this is a lane, or a road, is yet to be determined."""
+
+    def __init__(self, width: int, height: int) -> None:
+        """Initialize class instance."""
+
+        self.schedule = mesa.time.SimultaneousActivation(self)
+        self.grid = mesa.space.SingleGrid(width, height, torus=True)
